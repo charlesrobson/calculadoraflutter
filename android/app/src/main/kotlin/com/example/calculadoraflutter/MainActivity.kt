885f58a1c9a2e9091a14890a5d4f1310e9c5ce7b
Charles <secad.consig@gmail.com>
2020-11-17 23:05:42 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/calculadoraflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calculadoraflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+
+import 'package:calculadoraflutter/screens/calculator.dart';
+import 'package:flutter/material.dart';
+import 'screens/calculator.dart';
+
+
+void main() => runApp(Calculator());
